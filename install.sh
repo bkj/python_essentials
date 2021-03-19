d96aa4416f92cae7065bc27b7fd14536a30e70fc
Ubuntu <ubuntu@ip-172-31-81-211.ec2.internal>
2021-03-19 03:49:44 +0000

    updating install

--- install.sh
+++ install.sh
@@ -4,19 +4,31 @@
 
 # --
 # Install anaconda
+# 
 # !! For some reason, pytorch=1.8 install fails w/ AWS Deep Learning image pre-installed anaconda
-#    so we re-install it here. YMMV.
+#    so we re-install it here, and move the old one. YMMV.
 
+# # Move old conda
+# mv /home/ubuntu/anaconda3 /home/ubuntu/anaconda3_orig
+
+# # Install conda
 # wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
-# bash Anaconda3-2020.11-Linux-x86_64.sh -b -p /home/ubuntu/.anaconda
+# bash Anaconda3-2020.11-Linux-x86_64.sh -b -p /home/ubuntu/anaconda3
 # rm Anaconda3-2020.11-Linux-x86_64.sh
-# export PATH=/home/ubuntu/.anaconda/bin:$PATH
 
 # --
 # Set CUDA version to 11.0
 
 # sudo rm /usr/local/cuda
 # sudo ln -s /usr/local/cuda-11.0/ /usr/local/cuda
+
+# --
+# Upgrade cmake
+
+# wget https://github.com/Kitware/CMake/releases/download/v3.20.0-rc5/cmake-3.20.0-rc5-linux-x86_64.sh
+# bash cmake-3.20.0-rc5-linux-x86_64.sh
+# export PATH=$(pwd)/cmake-3.20.0-rc5-linux-x86_64/bin:$PATH
+# rm cmake-3.20.0-rc5-linux-x86_64.sh
 
 # --
 # Setup environment
