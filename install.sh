62028ff1d62d238c6516a4b00418af1bfc559b55
Ubuntu <ubuntu@ip-172-31-81-211.ec2.internal>
2021-03-19 03:40:44 +0000

    updating install

--- install.sh
+++ install.sh
@@ -4,10 +4,19 @@
 
 # --
 # Install anaconda
+# !! For some reason, pytorch=1.8 install fails w/ AWS Deep Learning image pre-installed anaconda
+#    so we re-install it here. YMMV.
 
 # wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
-# bash Anaconda3-2020.11-Linux-x86_64.sh
+# bash Anaconda3-2020.11-Linux-x86_64.sh -b -p /home/ubuntu/.anaconda
 # rm Anaconda3-2020.11-Linux-x86_64.sh
+# export PATH=/home/ubuntu/.anaconda/bin:$PATH
+
+# --
+# Set CUDA version to 11.0
+
+# sudo rm /usr/local/cuda
+# sudo ln -s /usr/local/cuda-11.0/ /usr/local/cuda
 
 # --
 # Setup environment
@@ -15,9 +24,8 @@
 conda create -y -n pyg_env python=3.7
 conda activate pyg_env
 
-# dependencies
-conda install -y -c pytorch -c conda-forge \
-    pytorch=1.8.0 cudatoolkit=11.1 
+conda install -y pytorch=1.8.0 cudatoolkit=11.1 \
+    -c pytorch -c conda-forge
 
 pip install scipy==1.6
 
